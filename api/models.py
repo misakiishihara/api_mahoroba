cb82c8fdf6b62a25c70c92333f2d3e82c4fc28db
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-04-11 19:14:10 +0900

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,41 @@
+#https://docs.djangoproject.com/en/4.2/ref/contrib/auth/
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+
+#
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('email is must')
+
+        user = self.model(email=self.normalize_email(email))
+        #パスワードをハッシュ化
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using= self._db)
+
+        return user
+#userモデルを定義
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    #adminへのアクセス権限を付与しない
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
